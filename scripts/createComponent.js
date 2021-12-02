74cbd69a4e941c5c5e5c8cdbde49f64a43621609
kyliu <liukaiyuan@deltaphone.com.cn>
2021-12-02 17:40:02 +0800

    添加快捷创建组件目录脚本

---
+++ scripts/createComponent.js
@@ -0,0 +1,76 @@
+/**
+ * 快捷创建组件目录
+ * 
+ * 使用：
+ *      npm run create  创建名字为 default 的组件目录
+ *      npm run create -- --name=xxx 创建名字为 xxx 的组件目录
+ */
+
+const PmsFs = require("fs/promises");
+const minimist = require("minimist");
+const path = require("path");
+
+const params = minimist(process.argv.slice(2));
+const DIRNAME = params.name || "default";
+
+const PATH = path.resolve(__dirname, DIRNAME);
+
+const files = [
+  {
+    name: `${DIRNAME}.vue`,
+    content: `
+<template></template>
+
+<script>
+export default {
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  created() {},
+  mounted() {},
+  watch: {},
+  methods: {},
+  components: {},
+};
+</script>
+    
+<style scoped lang="less">
+@import url(./${DIRNAME}.less);
+</style>
+`
+  },
+  {
+    name: "index.js",
+    content: `export { default } from './${DIRNAME}.vue'`
+  },
+  {
+    name: `${DIRNAME}.less`,
+    content: "less"
+  }
+];
+
+// 判断目录是否存在
+PmsFs.access(PATH)
+  .then(() => console.log(`创建失败，目录 ${PATH} 已存在`))
+  .catch(err => {
+    // 创建目录
+    PmsFs.mkdir(PATH)
+      .then(() => {
+        // 创建文件，写入内容
+        const arr = files.map(item => {
+          return PmsFs.writeFile(
+            path.resolve(__dirname, `${DIRNAME}/${item.name}`),
+            `${item.content}`
+          );
+        });
+
+        Promise.all(arr)
+          .then(() => {
+            console.log(`创建完成 ${PATH}`);
+          })
+          .catch(err => console.error(err));
+      })
+      .catch(err => console.error(err));
+  });
