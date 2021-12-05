c92ea74bb4e8db2b6b844849c4b1760446fa595c
kaka <1003345627@qq.com>
2021-12-05 22:55:07 +0800

    整理入口文件

---
+++ src/components/index.ts
@@ -0,0 +1,13 @@
+import type { App } from "vue";
+
+import Card from "@/components/Card";
+import { withInstall } from "@/utils";
+
+const compList = [Card];
+
+export function setupGlobalComponent(app: App<Element>) {
+  compList.forEach(comp => {
+    app.use(withInstall(comp));
+    // app.component(comp.name || comp.displayName, comp);
+  });
+}
