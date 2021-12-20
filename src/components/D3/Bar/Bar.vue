3542fd411e85b8857003b95e56995d87facb50cb
kaka <1003345627@qq.com>
2021-12-20 23:09:35 +0800

    添加 D3 的 types

--- src/components/D3/Bar/Bar.vue
+++ src/components/D3/Bar/Bar.vue
@@ -1,39 +1,38 @@
 
 <template>
-  <svg>
-    <rect></rect>
-    <rect></rect>
-    <rect></rect>
-    <rect></rect>
-  </svg>
+  <div class="container">
+    <svg />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted } from "vue";
-import * as D from "d3";
+import { defineComponent, onMounted } from "vue"
+import * as D from "d3"
 
 export default defineComponent({
   name: "Bar",
   setup() {
-    const arr = [1, 2, 3, 4];
+    const arr = [1, 2, 3, 4]
     onMounted(() => {
-      D.select("svg").style("background", "orange");
+      D.select("svg").style("background", "orange") 
 
-      D.selectAll("rect")
+      D.select("svg")
+        .selectAll("rect")
         .data(arr)
         .enter()
+        .append("rect")
         .attr("x", (d, i) => {
           console.log(123123)
-          return (i + 1) * 20;
+          return (i + 1) * 20
         })
         .attr("y", 20)
         .attr("width", 10)
         .attr("height", 10)
-        .attr("fill", "red");
-    });
-    return {};
+        .attr("fill", "red")
+    })
+    return {}
   },
-});
+})
 </script>
     
 <style lang="less" scoped>
